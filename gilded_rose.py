6aa2c46e114a9737970fd4c2d5c18a279a84162d
PAREKH <mihir.parekh@dxc.com>
2023-06-22 19:14:51 -0400

    Fixed classes to pass unit tests

--- gilded_rose.py
+++ gilded_rose.py
@@ -1,17 +1,17 @@
 # -*- coding: utf-8 -*-
 
 class GildedRose(object):
-
+    """Handling object """
     def __init__(self, items):
         self.items = items
-        
+
     def update_quality(self):
 
         for item in self.items:
             #my_item = Normal(item)
 
             if item.name == "Aged Brie":
-                my_item = AgedBrie(item) 
+                my_item = AgedBrie(item)
             elif item.name == "Backstage passes to a TAFKAL80ETC concert":
                 my_item = Concert(item)
             elif item.name == "Sulfuras, Hand of Ragnaros":
@@ -23,15 +23,18 @@ class GildedRose(object):
 
 
             my_item.virtual_update()
-        
+
 
 class Item:
+    """Item object"""
     def __init__(self, name, sell_in, quality):
         self.name = name
         self.sell_in = sell_in
         self.quality = quality
 
     def virtual_update(self):
+        """Pure virtual function which 
+        will be overwritten"""
         pass
 
     def __repr__(self):
@@ -39,75 +42,80 @@ class Item:
 
 
 class Normal(Item):
-    
+    """Class that inherits Item"""
     def __init__(self, item):
         self.item = item
-    
-    
+
     def virtual_update(self):
-        if self.item.sell_in < 0:
-            if self.item.quality > 0:
-                self.item.quality -= 2
-            else:
-                self.item.quailty = 0
-        
         self.item.sell_in -= 1
-        self.item.quality -=1         
+        if self.item.sell_in < 0:
+            self.item.quality -= 2
+        else:
+            self.item.quality -=1
+
+        if self.item.quality < 0:
+            self.item.quality = 0
+
 
 
 class AgedBrie(Item):
-
+    """Class that inherits Item"""
     def __init__(self, item):
         self.item = item
 
     def virtual_update(self):
         self.item.sell_in -= 1
-        self.item.quality +=1 
+        if self.item.sell_in < 0:
+            self.item.quality += 2
+        else:
+            self.item.quality +=1
+
         if self.item.quality > 50:
             self.item.quality = 50
 
 class Concert(Item):
-    
+    """Class that inherits Item"""
     def __init__(self, item):
-        self.item = item    
+        self.item = item
 
     def virtual_update(self):
+        self.item.sell_in -= 1
         if self.item.sell_in > 0:
-            if self.item.sell_in < 11:
+            if self.item.sell_in < 11 and self.item.sell_in > 5:
                 self.item.quality += 2
-        
+
             elif self.item.sell_in < 6:
                 self.item.quality += 3
-            
+
             else:
                 self.item.quality += 1
         else:
             self.item.quality = 0
+
         if self.item.quality > 50:
             self.item.quality = 50
 
-        self.item.sell_in -= 1      
 
 class Sulfuras(Item):
-
+    """Class that inherits Item"""
     def __init__(self, item):
         self.item = item
 
     def virtual_update(self):
+        self.item.sell_in = 0
         return
 
 class Conjured(Item):
-
+    """Class that inherits Item"""
     def __init__(self, item):
         self.item = item
 
     def virtual_update(self):
-        if self.item.sell_in < 0:
-            if self.item.quality > 0:
-                self.item.quality -= 4
-            else:
-                self.item.quailty = 0
-        
         self.item.sell_in -= 1
-        self.item.quality -=2  
+        if self.item.sell_in < 0:
+            self.item.quality -= 4
+        else:
+            self.item.quality -=2
 
+        if self.item.quality < 0:
+            self.item.quality = 0
