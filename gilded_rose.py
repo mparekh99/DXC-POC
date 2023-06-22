00b163a07d39ddee3eb6aef49972279687d98716
PAREKH <mihir.parekh@dxc.com>
2023-06-22 18:35:52 -0400

    now its orig lol

--- gilded_rose.py
+++ gilded_rose.py
@@ -5,16 +5,12 @@ class GildedRose(object):
     def __init__(self, items):
         self.items = items
 
-
     def update_quality(self):
-
         for item in self.items:
             if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert":
                 if item.quality > 0:
-                    if item.name != "Sulfuras, Hand of Ragnaros" and item.name != "Conjured Mana Cake":
+                    if item.name != "Sulfuras, Hand of Ragnaros":
                         item.quality = item.quality - 1
-                    if item.name == "Conjured Mana Cake":
-                        item.quality = item.quality - 2
             else:
                 if item.quality < 50:
                     item.quality = item.quality + 1
